85176428e246e483667b6b73de0354f9f1caadcc
Julian Pollinger <neferin42@gmail.com>
2020-09-30 16:07:38 +0200

    JSON validation

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app" class="container-fluid">
+  <div id="app" class="container-fluid pt-5">
       <form-root :json="schema" :ui="uischema"></form-root>
   </div>
 </template>
--- src/components/FormRoot.vue
+++ src/components/FormRoot.vue
@@ -1,34 +1,65 @@
 <template>
-  <b-form @submit="onSubmit">
+  <b-form @submit="onSubmit" v-if="valid || disableValidation">
     <FormWrap :json="json" :ui="ui"></FormWrap>
     <b-button :variant="colorVariant || 'primary'" type="submit">Send</b-button>
   </b-form>
+  <b-jumbotron v-else-if="validationResults" header="Error" bg-variant="danger" text-variant="white" lead="Validation of the form's schema failed with the following errors:">
+    <b-card class="error_card mb-3" v-for="(error, index) in validationResults.errors" :key="error.stack+index" :header="error.property">"{{error.instance}}" {{error.message}}</b-card>
+  </b-jumbotron>
 </template>
 
 <script>
 import FormWrap from "./FormWrap";
 import {onlyProps} from "./Layouts/layoutMixin";
+import schemadraft from "../json-schema_draft7.json";
 
 export default {
   name: "FormRoot",
   components: {FormWrap},
   mixins: [onlyProps],
+  data() {
+    return {
+      validationResults: null
+    }
+  },
+  computed: {
+    valid() {
+      return this.validationResults ? !this.validationResults.errors.length>0 : false;
+    }
+  },
   props: {
     //Color Variant like defined in Bootstrap-Vue
     colorVariant: {
       type: String,
       required: false
+    },
+    disableValidation: {
+      type: Boolean
     }
   },
   methods: {
     onSubmit(evt) {
       evt.preventDefault();
       console.log("send");
+    },
+    validateJson(json) {
+      const validate = require('jsonschema').validate;
+      this.validationResults = validate(json, schemadraft);
+    }
+  },
+  created() {
+    this.validateJson(this.json);
+  },
+  watch: {
+    json(newValue) {
+      this.validateJson(newValue);
     }
   },
 }
 </script>
 
 <style scoped>
-
+  .error_card {
+    color: black;
+  }
 </style>
