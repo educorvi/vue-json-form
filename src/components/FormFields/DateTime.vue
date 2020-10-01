168d5fb7f4744d78dec6c1c04e41b886effce718
Julian <neferin42@gmail.com>
2020-10-01 10:27:03 +0200

    DateTime

---
+++ src/components/FormFields/DateTime.vue
@@ -0,0 +1,25 @@
+<template>
+<div>
+  <b-row>
+    <b-col cols="6">
+      <b-input type="date"/>
+    </b-col>
+    <b-col cols="6">
+      <b-input type="time"/>
+    </b-col>
+  </b-row>
+</div>
+</template>
+
+<script>
+import formFieldMixin from "@/components/FormFields/formFieldMixin";
+
+export default {
+name: "DateTime",
+  mixins: [formFieldMixin]
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/components/FormFields/String.vue
+++ src/components/FormFields/String.vue
@@ -1,13 +1,17 @@
 <template>
-  <b-form-textarea v-if="ui.options && ui.options.multi" :rows="typeof ui.options.multi === 'number' ? ui.options.multi : 3"/>
-  <b-input :type="item.format" v-else/>
+  <b-form-textarea v-if="ui.options && ui.options.multi"
+                   :rows="typeof ui.options.multi === 'number' ? ui.options.multi : 3"/>
+  <b-input v-else-if="item.format !== 'date-time'" :type="item.format"/>
+  <date-time v-else :json="json" :ui="ui"/>
 </template>
 
 <script>
 import formFieldMixin from "./formFieldMixin";
+import DateTime from "@/components/FormFields/DateTime";
 
 export default {
   name: "String",
+  components: {DateTime},
   mixins: [formFieldMixin]
 }
 </script>
