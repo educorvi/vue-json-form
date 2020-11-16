ad504c513798c320600fdc2f37ba3c339fbe0daa
Julian Pollinger <neferin42@gmail.com>
2020-11-16 15:16:30 +0100

    Number Max/Min

--- src/components/FormFields/Number.vue
+++ src/components/FormFields/Number.vue
@@ -1,7 +1,7 @@
 <template>
   <b-form-input ref="child" v-if="!ui.options || !ui.options.rating" v-model="fieldData" :name="title" :placeholder="ui.options ? ui.options.placeholder : ''"
-                :required="required" :step="isInteger ? 1 : (item.multipleOf || Math.pow(10, -10))"
-                type="number"></b-form-input>
+                :required="required" :step="item.multipleOf || 1"
+                type="number" :min="item.minimum" :max="item.maximum"></b-form-input>
   <b-form-rating ref="child" v-else v-model="fieldData" :name="title" :required="required" :stars="item.maximum || 5"
                  variant="warning"></b-form-rating>
 </template>
