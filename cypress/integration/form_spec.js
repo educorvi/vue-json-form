a5e11d39293f60681afd511d357a2d6f9a1c13f2
Julian Pollinger <julian.pollinger@jp-studios.de>
2021-03-15 10:12:20 +0100

    tests enhanced

--- cypress/integration/form_spec.js
+++ cypress/integration/form_spec.js
@@ -8,7 +8,7 @@ beforeEach(() => {
     cy.visit("http://localhost:8080/");
     cy.contains("Great Switch").click();
     cy.get("#name").type(daten.name);
-    cy.get("#fancyness").first().click();
+    cy.get("#fancyness :nth-child(4) > input").click({force: true});
 });
 
 describe("Check if everything is there and v-models work", () => {
@@ -27,16 +27,16 @@ describe("Check if everything is there and v-models work", () => {
             expect(name).to.equal(daten.name);
             expect(title).to.equal(daten.title);
             expect(done).to.be.true;
-            expect(fancyness).to.equal("fanciest");
+            expect(fancyness).to.equal("unicorn");
 
         });
     });
     it('Groups', () => {
         cy.get("#due_date");
         //Select four Stars in Rating
-        cy.get(':nth-child(4) > .b-rating-icon > .bi-star > g > path').click({force: true});
-        cy.get('[type="date"]').type('2021-12-28');
-        cy.get('[type="time"]').type('23:24');
+        cy.get('#rating :nth-child(4) > .b-rating-icon > .bi-star > g > path').click({force: true});
+        cy.get('#due_date > [type="date"]').type('2021-12-28');
+        cy.get('#due_date > [type="time"]').type('23:24');
         cy.get('#description').type(daten.longText);
         cy.get("#weekday_BV_option_0").check({force: true});
         cy.get("#weekday_BV_option_2").check({force: true});
