3a6a3da008ac6f8c4f50a9514a5d598dd358e8b0
Julian Pollinger <julian@pollinger.dev>
2023-08-15 13:12:51 +0200

    ci

--- cypress.config.js
+++ cypress.config.js
@@ -4,10 +4,5 @@ module.exports = defineConfig({
   experimentalStudio: true,
   videoCompression: false,
   e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
-    setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
-    },
   },
 })
