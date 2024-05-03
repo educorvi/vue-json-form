025473e713d3ab8a5dc02c50e07e8eebc1724428
Julian Pollinger <julian@pollinger.dev>
2024-05-03 14:03:52 +0200

    options for file upload

--- vue-json-form/src/renderings/bootstrap/controls/FileControl.vue
+++ vue-json-form/src/renderings/bootstrap/controls/FileControl.vue
@@ -4,6 +4,7 @@ import { storeToRefs } from 'pinia';
 import { useFormDataStore } from '@/stores/formData';
 import { injectJsonData } from '@/computedProperties/json';
 import { controlID } from '@/computedProperties/misc';
+
 const { formData } = storeToRefs(useFormDataStore());
 
 const { layoutElement, jsonElement, savePath } = injectJsonData();
@@ -11,7 +12,13 @@ const id = controlID(savePath);
 </script>
 
 <template>
-    <BFormFile v-model="formData[savePath]" :id="id" class="vjf_file" />
+    <BFormFile
+        v-model="formData[savePath]"
+        :id="id"
+        class="vjf_file"
+        :multiple="layoutElement.options?.allowMultipleFiles"
+        :accept="layoutElement.options?.acceptedFileType"
+    />
 </template>
 
 <style scoped></style>
