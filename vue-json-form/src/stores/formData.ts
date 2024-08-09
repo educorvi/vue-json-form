0e343071bde56e7d0264ddce791de4ce8e0f4275
Julian Pollinger <julian@pollinger.dev>
2024-08-09 09:44:39 +0200

    document formData.ts

--- vue-json-form/src/stores/formData.ts
+++ vue-json-form/src/stores/formData.ts
@@ -1,56 +1,98 @@
-import { defineStore, storeToRefs } from 'pinia';
-import type { CoreSchemaMetaSchema } from '@/typings/json-schema';
-import { useFormStructureStore } from '@/stores/formStructure';
-import { cleanScope, isArray } from '@/computedProperties/json';
-import type { FunctionArgs } from '@vueuse/core';
+import { defineStore } from 'pinia';
+import { isArray } from '@/computedProperties/json';
 
+/**
+ * Sets a property on an object based on a scoped key.
+ *
+ * This function takes an object, a scoped key (which represents a path), and a value.
+ * It then sets the value at the specified path within the object, creating nested
+ * objects or arrays as necessary.
+ *
+ * @param object - The object on which the property will be set.
+ * @param key - The scoped key representing the path where the value should be set.
+ * @param value - The value to set at the specified path.
+ **/
 function setPropertyByScope(
     object: Record<any, any>,
     key: string,
     value: any
 ): void {
+    // Do not write anything if the value is undefined
     if (value === undefined) {
         return;
     }
-    // key = key.replace(/\[(\w+)]/g, '/$1'); // convert indexes to properties
 
-    const a = key
+    // Split the key by '/' and filter out empty strings and 'properties'
+    // at even indices to get the actual path from the path in the json schema
+    const splitKey = key
         .split('/')
         .filter((x) => x !== '')
         .filter((x, index) => !(index % 2 === 0 && x === 'properties'));
 
+    // Regular expression to match array indices
     const arrayIndexRegex = /\[(\w+)]/;
 
-    for (let i = 0; i < a.length; i++) {
-        const k = a[i];
+    // Create necessary nested objects or arrays and set the value at the specified path
+    for (let i = 0; i < splitKey.length; i++) {
+        const k = splitKey[i];
         const indexMatch = k.match(arrayIndexRegex);
+
         if (indexMatch) {
+            // Key is an array index
+
+            // Get name of array and index
             const index = parseInt(indexMatch[1]);
             const arrayName = k.replace(arrayIndexRegex, '');
+
+            // Create array if it doesn't exist
             if (!(arrayName in object)) {
                 object[arrayName] = [];
             }
-            if (i === a.length - 1) {
+
+            // If this is the last key, set the value
+            if (i === splitKey.length - 1) {
                 object[arrayName][index] = value;
                 return;
             }
+
+            // If this is not the last key, create a new object
             if (object[arrayName][index] === undefined) {
                 object[arrayName][index] = Object.create(null);
             }
+
+            // Move to newly created object
             object = object[arrayName][index];
-        } else if (!(k in object)) {
-            if (i === a.length - 1) {
-                object[a[a.length - 1]] = value;
+        } else {
+            // Key is an object key
+
+            // Create object if it doesn't exist
+            if (!(k in object)) {
+                object[k] = Object.create(null);
+            }
+
+            // If this is the last key, set the value
+            if (i === splitKey.length - 1) {
+                object[splitKey[splitKey.length - 1]] = value;
                 return;
             } else {
-                object[k] = Object.create(null);
+                // Move to newly created object
                 object = object[k];
             }
         }
     }
 }
 
-function reduceObjects(
+/**
+ * Converts a flat object with scoped keys into a nested object.
+ *
+ * This function takes an object where keys represent paths (scopes) and values
+ * represent the data at those paths. It then constructs a nested object based
+ * on these paths.
+ *
+ * @param obj - A readonly object where keys are scoped paths and values are the data.
+ * @returns A nested object constructed from the scoped paths.
+ **/
+function reduceToObject(
     obj: Readonly<Record<string, any>>
 ): Record<string, any> {
     const ret = {};
@@ -62,10 +104,20 @@ function reduceObjects(
     return ret;
 }
 
-function cleanData(obj: Readonly<Record<string, any>>): Record<string, any> {
+/**
+ * Cleans the data by mapping the array entry values to their indices
+ * @param obj - The object to clean
+ * @returns The cleaned data in scopes formatting and as json object
+ */
+function cleanData(obj: Readonly<Record<string, any>>): {
+    scopes: Record<string, any>;
+    json: Record<string, any>;
+} {
     const scopes: Record<string, any> = {};
     const arrayIndices: Map<string, number> = new Map();
     const arrays = new Set<string>();
+
+    // Create a map of all array values and their indices
     for (const [key, value] of Object.entries(obj)) {
         if (isArray(key)) {
             arrays.add(key);
@@ -76,6 +128,8 @@ function cleanData(obj: Readonly<Record<string, any>>): Record<string, any> {
             }
         }
     }
+
+    // Replace the placeholder in the scopes with the actual index
     for (const [key, value] of Object.entries(obj)) {
         if (!isArray(key)) {
             let newKey = key;
@@ -86,7 +140,7 @@ function cleanData(obj: Readonly<Record<string, any>>): Record<string, any> {
         }
     }
 
-    const json = reduceObjects(scopes);
+    const json = reduceToObject(scopes);
     return { scopes, json };
 }
 
