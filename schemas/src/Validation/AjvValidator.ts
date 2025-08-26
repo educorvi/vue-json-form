4f30ac0462adcfa90ecf43c08824163e456f11bd
Julian Pollinger <julian@pollinger.dev>
2025-08-26 16:56:58 +0200

    fix: fix ui schema

--- schemas/src/Validation/AjvValidator.ts
+++ schemas/src/Validation/AjvValidator.ts
@@ -8,11 +8,13 @@ export class AjvValidator extends Validator<ErrorObject>{
     jsonValidationFunc: ValidateFunction | undefined;
     uiValidationFunc: ValidateFunction | undefined;
     protected async initializeInternal(): Promise<void> {
-        const { default: Ajv } = await import('ajv');
+        const { default: Ajv } = await import('ajv/dist/2020.js');
         const {default: addFormats} = await import('ajv-formats');
+        const draft7MetaSchema = (await import('ajv/dist/refs/json-schema-draft-07.json')).default;
         const uiSchema = (await import('../generated/ui-merged.schema.json')).default;
         const ajv = new Ajv();
         addFormats(ajv);
+        ajv.addMetaSchema(draft7MetaSchema);
         this.jsonValidationFunc =
             ajv.getSchema('https://json-schema.org/draft-07/schema') ??
             ajv.getSchema('http://json-schema.org/draft-07/schema');
