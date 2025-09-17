3e9639712e9ae3b8d6989b2a83b36e9ffba1c5c2
Julian Pollinger <julian@pollinger.dev>
2025-09-17 15:45:22 +0200

    feat: allow minimum and maximum number of files for fileUpload field

--- vue-json-form/src/renderings/bootstrap/controls/FileControl.vue
+++ vue-json-form/src/renderings/bootstrap/controls/FileControl.vue
@@ -1,16 +1,51 @@
 <script setup lang="ts">
-/* eslint-disable vue/no-deprecated-filter */
 import { BFormFile } from 'bootstrap-vue-next';
 import { storeToRefs } from 'pinia';
 import { useFormDataStore } from '@/stores/formData';
 import { injectJsonData } from '@/computedProperties/json';
 import { controlID } from '@/computedProperties/misc';
 import { getOption } from '@/utilities';
+import { computed, inject, useTemplateRef, watch } from 'vue';
+import { languageProviderKey } from '@/components/ProviderKeys.ts';
 
 const { formData } = storeToRefs(useFormDataStore());
 
 const { layoutElement, jsonElement, savePath } = injectJsonData();
 const id = controlID(savePath);
+
+const languageProvider = inject(languageProviderKey);
+
+watch(
+    () => formData.value[savePath],
+    (newVal) => {
+        validateInput(newVal);
+    },
+    { deep: true }
+);
+
+function validateInput(data: any) {
+    const { allowMultipleFiles, maxNumberOfFiles, minNumberOfFiles } =
+        layoutElement.options || {};
+    if (allowMultipleFiles) {
+        const el = document.getElementById(id.value) as HTMLInputElement;
+        if (maxNumberOfFiles && data.length > maxNumberOfFiles) {
+            el.setCustomValidity(
+                languageProvider?.getStringTemplate(
+                    'errors.fileUpload.tooManyFiles',
+                    maxNumberOfFiles
+                ) || ''
+            );
+        }
+        if (minNumberOfFiles && data.length < minNumberOfFiles) {
+            el.setCustomValidity(
+                languageProvider?.getStringTemplate(
+                    'errors.fileUpload.tooFewFiles',
+                    minNumberOfFiles
+                ) || ''
+            );
+        }
+    }
+}
 </script>
 
 <template>
