eb4abe2ccb4973bcc0e0ae2c97693e08b3bfc15a
Julian Pollinger <julian@pollinger.dev>
2025-11-13 14:09:13 +0100

    refactor: cleanup

--- vue-json-form/vite.dev.config.ts
+++ vue-json-form/vite.dev.config.ts
@@ -1,7 +1,4 @@
-import { fileURLToPath, URL } from 'node:url';
-
 import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
 import { mergeConfig } from 'vitest/config';
 import vjfConfig from './vite.config';
 
