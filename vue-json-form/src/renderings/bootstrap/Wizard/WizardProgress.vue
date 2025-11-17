5e6a9dca1e0c5a00eaf0000d6475ec925b52f7f1
Julian Pollinger <julian@pollinger.dev>
2025-11-17 18:09:58 +0100

    chore: disfunctiomal offset

--- vue-json-form/src/renderings/bootstrap/Wizard/WizardProgress.vue
+++ vue-json-form/src/renderings/bootstrap/Wizard/WizardProgress.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { BProgress, BRow, BCol } from 'bootstrap-vue-next';
+import { watch } from 'vue';
 const props = defineProps<{
     max: number;
     pageNames?: string[];
@@ -12,42 +13,127 @@ function clickedStep(step: number) {
         currentStep.value = index;
     }
 }
+watch(currentStep, () => {
+    Array.from(
+        document.querySelectorAll<HTMLTableElement>('.custom-wizard-progress')
+    ).forEach((el: HTMLElement) => {
+        const progress = (currentStep.value / (props.max - 1)) * 100;
+        el.style.setProperty('--progress', `${progress}%`);
+        el.style.setProperty('--offset', `${progress / 100}%`);
+    });
+});
 </script>
 <template>
-    <div class="stepWrapper">
-        <div class="d-flex justify-content-between">
-            <div
-                :class="{
-                    'stepNumber d-flex justify-content-center align-items-center': true,
-                    active: stepNumber - 1 === currentStep,
-                    filled: stepNumber - 1 < currentStep,
-                }"
-                v-for="stepNumber in Array.from(
-                    { length: props.max },
-                    (_, i) => i + 1
-                )"
-            >
-                <button
-                    :disabled="stepNumber > currentStep"
-                    class="fs-1"
-                    @click="() => clickedStep(stepNumber)"
+    <div class="wrappers-parent">
+        <div class="stepWrapper">
+            <div class="d-flex justify-content-between">
+                <div
+                    :class="{
+                        'stepNumber d-flex justify-content-center align-items-center': true,
+                        active: stepNumber - 1 === currentStep,
+                        filled: stepNumber - 1 < currentStep,
+                    }"
+                    v-for="stepNumber in Array.from(
+                        { length: props.max },
+                        (_, i) => i + 1
+                    )"
                 >
-                    {{ stepNumber }}
-                </button>
+                    <button
+                        :disabled="stepNumber > currentStep"
+                        class="fs-4"
+                        @click="() => clickedStep(stepNumber)"
+                    >
+                        {{ stepNumber }}
+                    </button>
+                </div>
             </div>
+            <!--        <BProgress class="stepProgress" :value="currentStep" :max="max - 1" />-->
         </div>
-        <BProgress class="stepProgress" :value="currentStep" :max="max - 1" />
+        <div class="custom-wizard-progress"></div>
+        <div class="stepWrapperBg">
+            <div class="d-flex justify-content-between">
+                <div
+                    :class="{
+                        'stepNumber d-flex justify-content-center align-items-center': true,
+                        active: stepNumber - 1 === currentStep,
+                        filled: stepNumber - 1 < currentStep,
+                    }"
+                    v-for="stepNumber in Array.from(
+                        { length: props.max },
+                        (_, i) => i + 1
+                    )"
+                ></div>
+            </div>
+            <!--        <BProgress class="stepProgress" :value="currentStep" :max="max - 1" />-->
+        </div>
     </div>
 </template>
 
 <style scoped lang="scss">
 $step-border-radius: 50%;
-$step-size: 60px;
+$step-size: 40px;
+$bar-size: 13px;
 
-.stepWrapper {
+.wrappers-parent {
     position: relative;
     margin-left: 40px;
     margin-right: 40px;
+}
+.stepWrapper {
+    position: relative;
+    z-index: 5;
+    //visibility: hidden;
+}
+
+.custom-wizard-progress {
+    --progress: 0%;
+    --offset: 0%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: var(--bs-primary);
+    mix-blend-mode: lighten;
+
+    &::after {
+        content: '';
+        position: absolute;
+        top: 0;
+        left: calc(var(--progress) + (var(--offset)));
+        width: calc(100% - var(--progress) - (var(--offset)));
+        height: 100%;
+        background: var(--bs-secondary-bg);
+        transition: all 0.4s ease-in-out;
+    }
+}
+
+.stepWrapperBg {
+    background: var(--bs-body-bg);
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: -1;
+
+    & .stepNumber {
+        background: black;
+        border: none;
+    }
+
+    &::before {
+        content: '';
+        position: absolute;
+        top: 50%;
+        left: calc($step-size/2);
+        width: calc(100% - $step-size);
+        height: $bar-size;
+        background: black;
+        transform: translateY(-50%);
+        padding-left: calc($step-size/2);
+        padding-right: calc($step-size/2);
+    }
 }
 .stepProgress {
     position: absolute;
@@ -71,37 +157,8 @@ $step-size: 60px;
     width: $step-size;
     height: $step-size;
     border-radius: $step-border-radius;
-
-    &::after {
-        content: '';
-        position: absolute;
-        height: calc(100% + 10px);
-        width: calc(100% + 10px);
-        //background: gray;
-        background-image: linear-gradient(
-            to left,
-            var(--bs-secondary-bg) var(--fadePercent),
-            var(--bs-primary)
-        );
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%);
-        z-index: -1;
-        border-radius: $step-border-radius;
-        transition: --fadePercent 0.1s;
-        transition-delay: 0s;
-    }
-
-    &.active::after {
-        --fadePercent: 38%;
-        transition-delay: 0.3s !important;
-        transition: --fadePercent 0.5s;
-    }
-    &.filled::after {
-        --fadePercent: -1000%;
-        transition-delay: 0s;
-        transition: --fadePercent 0.5s;
-    }
+    border: 4px solid transparent;
+    background-clip: content-box;
 
     & > button {
         background: transparent;
