db79cf81041637834e3fa48414135d25db54faf0
Julian Pollinger <julian@pollinger.dev>
2025-12-02 12:07:32 +0100

    fix: fix shadow root styling

--- webcomponent/src/vite-env.d.ts
+++ webcomponent/src/vite-env.d.ts
@@ -2,3 +2,7 @@ declare module '*.css?inline' {
     const content: string;
     export default content;
 }
+declare module '*.scss?inline' {
+    const content: string;
+    export default content;
+}
