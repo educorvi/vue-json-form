2230ece3f398f4457e2f7f6a9417c5cfa2563845
Julian Pollinger <julian@pollinger.dev>
2025-12-02 10:41:13 +0100

    feat: multiple webcomponent variants

---
+++ webcomponent/src/vueComponentCommons.ts
@@ -0,0 +1,53 @@
+import type { SubmitOptions } from '@educorvi/vue-json-form-schemas';
+import { computed, defineProps, defineEmits, type PropType, type DefineProps } from 'vue';
+import axios from 'axios';
+
+export type Props = {
+    /**
+     * The JSON Schema of the form
+     */
+    jsonSchema: string;
+
+    /**
+     * The UI Schema of the form
+     */
+    uiSchema?: string;
+
+    /**
+     * The plain data of the form
+     */
+    presetData?: string;
+
+    /**
+     * Return data as key value pairs with the keys being the scopes as used in the ui schema and the values being the data
+     */
+    returnDataAsScopes?: boolean | string;
+}
+
+export type Emits = {
+    (e: 'submit', data: Record<string, any>, options: SubmitOptions): void;
+    (e: 'afterSubmitted', data: Record<string, any>, options: SubmitOptions): void;
+}
+
+export function getComputed(props: DefineProps<Props, never>) {
+    const jsonSchema = computed(() => JSON.parse(props.jsonSchema) as Record<string, any>);
+    const uiSchema = computed(() => props.uiSchema ? JSON.parse(props.uiSchema) as Record<string, any> : undefined);
+    const presetData = computed(() => props.presetData ? JSON.parse(props.presetData) as Record<string, any> : undefined);
+    const returnDataAsScopes = computed(() => props.returnDataAsScopes === true || props.returnDataAsScopes === 'true');
+    return { jsonSchema, uiSchema, presetData, returnDataAsScopes };
+}
+
+export function getSubmitFunc(emit: Emits) {
+    return async function onSubmitForm(data: Record<string, any>, options: SubmitOptions) {
+        if (options.action === 'request' && options.request?.url) {
+            await axios(options.request.url, {
+                method: options.request.method || 'POST',
+                headers: options.request.headers,
+                data,
+            });
+        } else {
+            emit('submit', data, options);
+        }
+        emit('afterSubmitted', data, options);
+    };
+}
--- webcomponent/vite.config.ts
+++ webcomponent/vite.config.ts
@@ -1,30 +1,36 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
-export default defineConfig({
-    plugins: [
-        vue({
-            template: {
-                compilerOptions: {
-                    isCustomElement: (tag: any) => tag.includes('vue-json-form'),
+export function getConfig(entry: string, outDir: string): Parameters<typeof defineConfig>[0] {
+    return {
+        plugins: [
+            vue({
+                template: {
+                    compilerOptions: {
+                        isCustomElement: (tag: any) => tag.includes('vue-json-form'),
+                    },
                 },
+            }),
+        ],
+        build: {
+            sourcemap: true,
+            minify: 'esbuild',
+            lib: {
+                entry,
+                name: 'vue-json-form',
+                // the proper extensions will be added
+                fileName: 'vue-json-form',
+                formats: ['umd', 'es'],
             },
-        }),
-    ],
-    build: {
-        sourcemap: true,
-        minify: 'esbuild',
-        lib: {
-            entry: './src/main.ce.ts',
-            name: 'vue-json-form',
-            // the proper extensions will be added
-            fileName: 'vue-json-form',
-            formats: ['umd', 'es'],
+            outDir,
         },
-        outDir: 'dist',
-    },
-    define: {
-        'process.env.NODE_ENV': JSON.stringify('production'),
-        // "process.env.NODE_ENV": JSON.stringify("development"),
-    },
-});
+        define: {
+            'process.env.NODE_ENV': JSON.stringify('production'),
+            // "process.env.NODE_ENV": JSON.stringify("development"),
+        },
+    }
+}
+
+export default defineConfig(
+    getConfig('./src/main.ce.ts', './dist/default')
+);
