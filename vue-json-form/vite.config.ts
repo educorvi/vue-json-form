920c49b0f50ebddb06792199dad9490e93b924e3
Julian Pollinger <julian.pollinger@educorvi.de>
2025-12-03 14:31:16 +0100

    build: improve dependency management and build size (#100)

--- vue-json-form/vite.config.ts
+++ vue-json-form/vite.config.ts
@@ -5,9 +5,9 @@ import vue from '@vitejs/plugin-vue';
 import { resolve } from 'path';
 import dts from 'vite-plugin-dts';
 import pkg from './package.json';
+import { externalizeDeps } from 'vite-plugin-externalize-deps';
 // import { nodePolyfills } from 'vite-plugin-node-polyfills';
 import vueDevTools from 'vite-plugin-vue-devtools';
-import ReactivityTransform from '@vue-macros/reactivity-transform/vite'
 
 export default defineConfig({
     define: {
@@ -35,12 +35,11 @@ export default defineConfig({
             name: 'VueJsonForm',
             // the proper extensions will be added
             fileName: 'vue-json-form',
-            formats: ['es', 'umd'],
+            formats: ['es', 'cjs'],
         },
         rollupOptions: {
             // make sure to externalize deps that shouldn't be bundled
             // into your library
-            external: ['vue'],
             output: {
                 // Provide global variables to use in the UMD build
                 // for externalized deps
@@ -62,6 +61,7 @@ export default defineConfig({
         //     },
         // }),
         // vueDevTools(),
+        externalizeDeps(),
         vue({
             include: [/\.vue$/],
         }),
@@ -72,7 +72,6 @@ export default defineConfig({
             insertTypesEntry: true,
             copyDtsFiles: true,
         }),
-        ReactivityTransform(),
     ],
     css: {
         preprocessorOptions: {
