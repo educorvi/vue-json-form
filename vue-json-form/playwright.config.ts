56f71000f4f508bd39cb3690a6687c7b0fab296b
Julian Pollinger <julian@pollinger.dev>
2025-12-12 18:17:45 +0100

    test: test more browsers

--- vue-json-form/playwright.config.ts
+++ vue-json-form/playwright.config.ts
@@ -33,6 +33,16 @@ export default defineConfig({
             name: 'chromium',
             use: { ...devices['Desktop Chrome'] },
         },
+        {
+            name: 'firefox',
+            use: { ...devices['Desktop Firefox'] },
+        },
+
+        /* Test against mobile viewports. */
+        {
+            name: 'Mobile Chrome',
+            use: { ...devices['Pixel 5'] },
+        },
     ],
 
     /* Run your local dev server before starting the tests */
