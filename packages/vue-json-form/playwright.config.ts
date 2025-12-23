f99b25107e86d5d6034cfcd7fe02260723967f58
Julian Pollinger <julian@pollinger.dev>
2025-12-23 10:31:50 +0100

    ci: update Playwright config and CI for enhanced test artifacts retention

--- packages/vue-json-form/playwright.config.ts
+++ packages/vue-json-form/playwright.config.ts
@@ -26,7 +26,10 @@ export default defineConfig({
         /* Base URL to use in actions like `await page.goto('/')`. */
         baseURL: 'http://localhost:5173',
         /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-        trace: 'on-first-retry',
+        trace: 'retain-on-first-failure',
+
+        screenshot: 'on-first-failure',
+        video: 'retain-on-failure',
     },
 
     /* Configure projects for major browsers */
