39520b99f2107d0459b8e803c55cc62fc7f66684
Julian Pollinger <julian@pollinger.dev>
2026-01-13 17:18:59 +0100

    test: improve accessibility test setup

--- packages/vue-json-form/tests/e2e/accessibility.spec.ts
+++ packages/vue-json-form/tests/e2e/accessibility.spec.ts
@@ -2,27 +2,31 @@ import { test, expect } from '@playwright/test';
 import AxeBuilder from '@axe-core/playwright';
 
 test.describe('Check for accessibility issues', () => {
-  const axeOptions = {
-    rules: {
-      'page-has-heading-one': { enabled: false },
-    },
-  };
+    const axeOptions = {
+        rules: {
+            'page-has-heading-one': { enabled: false },
+        },
+    };
 
-  test('Showcase', async ({ page }) => {
-    await page.goto('http://localhost:5173/showcase?nonav=true');
-    const accessibilityScanResults = await new AxeBuilder({ page })
-      .options(axeOptions)
-      .analyze();
+    test('Showcase', async ({ page }) => {
+        await page.goto('http://localhost:5173/showcase?nonav=true');
+        // Wait for page to render
+        expect(await page.locator('input').count()).toBeGreaterThan(0);
+        const accessibilityScanResults = await new AxeBuilder({ page })
+            .options(axeOptions)
+            .analyze();
 
-    expect(accessibilityScanResults.violations).toEqual([]);
-  });
+        expect(accessibilityScanResults.violations).toEqual([]);
+    });
 
-  test('Reproduce', async ({ page }) => {
-    await page.goto('http://localhost:5173/reproduce?nonav=true');
-    const accessibilityScanResults = await new AxeBuilder({ page })
-      .options(axeOptions)
-      .analyze();
+    test('Reproduce', async ({ page }) => {
+        await page.goto('http://localhost:5173/reproduce?nonav=true');
+        // Wait for page to render
+        expect(await page.locator('input').count()).toBeGreaterThan(0);
+        const accessibilityScanResults = await new AxeBuilder({ page })
+            .options(axeOptions)
+            .analyze();
 
-    expect(accessibilityScanResults.violations).toEqual([]);
-  });
+        expect(accessibilityScanResults.violations).toEqual([]);
+    });
 });
