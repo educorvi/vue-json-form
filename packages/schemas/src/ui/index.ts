85b9c4664279dfa50dfbf217fce0fde57406e5bb
Julian Pollinger <julian@pollinger.dev>
2026-01-30 16:56:51 +0100

    fix: add wizard schema to allSchemas lists

--- packages/schemas/src/ui/index.ts
+++ packages/schemas/src/ui/index.ts
@@ -7,6 +7,7 @@ import layoutSchema from './layout.schema.json';
 import showOnSchema from './show_on.schema.json';
 import uiSchema from './ui.schema.json';
 import variantsSchema from './variants.schema.json';
+import wizard from './wizard.schema.json';
 import RefParser from '@apidevtools/json-schema-ref-parser';
 
 export const allSchemas = [
@@ -19,6 +20,7 @@ export const allSchemas = [
     showOnSchema,
     uiSchema,
     variantsSchema,
+    wizard
 ];
 
 export const allSchemasWithIncludedRita = [
@@ -31,4 +33,5 @@ export const allSchemasWithIncludedRita = [
     RefParser.dereference(showOnSchema),
     uiSchema,
     variantsSchema,
+    wizard
 ];
