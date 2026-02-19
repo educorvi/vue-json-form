f35f106a0752ba50f3914ccc438a98608d6e518d
Julian Pollinger <julian@pollinger.dev>
2026-02-19 18:37:41 +0100

    test: add separate test scripts for unit and e2e testing in TurboRepo

--- apps/demo/tests/e2e/example.spec.ts
+++ apps/demo/tests/e2e/example.spec.ts
@@ -3,6 +3,6 @@ import { test, expect } from '@playwright/test';
 test.describe('My First Test', () => {
   test('visits the app root url', async ({ page }) => {
     await page.goto('/');
-    await expect(page.locator('h1')).toContainText('You did it!');
+    await expect(page.locator('main > h2:first-child')).toContainText('VJF Demo');
   });
 });
