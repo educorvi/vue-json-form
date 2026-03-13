0a62539e08ecd0e328a5956022622447febd914c
Julian Pollinger <julian@pollinger.dev>
2026-03-13 17:26:53 +0100

    chore: todo

--- packages/vue-json-form/src/computedProperties/css.ts
+++ packages/vue-json-form/src/computedProperties/css.ts
@@ -1,18 +1,19 @@
-import { computed } from 'vue';
+import { computed, type Ref } from 'vue';
 import { hasCssClass } from '@/typings/typeValidators';
+import type { LayoutElement } from '@educorvi/vue-json-form-schemas';
+import { getOption } from '@/utilities.ts';
 
 /**
  * Computed CSS class for the given element
  * @param element The element
  * @param defaultClasses The plain classes
  */
-export function computedCssClass(element: any, ...defaultClasses: string[]) {
+export function computedCssClass(
+    element: Ref<LayoutElement>,
+    ...defaultClasses: string[]
+) {
     return computed(() => {
         const defaultClassString = ' ' + defaultClasses.join(' ');
-        if (hasCssClass(element)) {
-            return element.options.cssClass.toString() + defaultClassString;
-        } else {
-            return defaultClassString;
-        }
+        return getOption(element.value, 'cssClass', '') + defaultClassString;
     });
 }
--- packages/vue-json-form/src/renderings/bootstrap/controls/ObjectControl.vue
+++ packages/vue-json-form/src/renderings/bootstrap/controls/ObjectControl.vue
@@ -1,50 +1,70 @@
 <script setup lang="ts">
 import { computedLabel, injectJsonData } from '@/computedProperties/json';
 import { controlID } from '@/computedProperties/misc';
-import { onMounted, ref, watch } from 'vue';
+import { computed, onMounted, ref, watch } from 'vue';
 import { generateUISchema } from '@/Commons';
 import FormWrap from '@/components/FormWrap.vue';
 import type { Layout } from '@educorvi/vue-json-form-schemas';
+import VerticalLayout from '@/components/LayoutElements/VerticalLayout.vue';
+import { computedCssClass } from '@/computedProperties/css.ts';
 
 const { jsonElement, layoutElement, savePath } = injectJsonData();
 const id = controlID(savePath);
+const cssClass = computedCssClass(layoutElement, 'vjf_object');
+const label = computedLabel(layoutElement);
 
-const layout = ref(undefined as Layout | undefined);
-
-function setLayout() {
-    const generationOptions = {
-        scopeBase: layoutElement.value.scope,
-        layoutType: 'Group' as const,
-        groupLabel:
-            layoutElement.value.options?.label !== false
-                ? computedLabel(layoutElement).value
-                : '',
-        groupDescription: jsonElement.value.description,
-    };
-    const uiSchema = generateUISchema(jsonElement.value, generationOptions);
-    if (!layout.value) {
-        layout.value = uiSchema.layout;
-    } else {
-        if (JSON.stringify(layout.value) !== JSON.stringify(uiSchema.layout)) {
-            layout.value = uiSchema.layout;
-        }
-    }
-}
-
-watch([() => jsonElement.value, () => layoutElement.value], setLayout, {
-    deep: true,
-});
-onMounted(setLayout);
+// const layout = ref(undefined as Layout | undefined);
+//
+// function setLayout() {
+//     const generationOptions = {
+//         scopeBase: layoutElement.value.scope,
+//         layoutType: 'Group' as const,
+//         groupLabel:
+//             layoutElement.value.options?.label !== false
+//                 ? computedLabel(layoutElement).value
+//                 : '',
+//         groupDescription: jsonElement.value.description,
+//     };
+//     const uiSchema = generateUISchema(jsonElement.value, generationOptions);
+//     if (!layout.value) {
+//         layout.value = uiSchema.layout;
+//     } else {
+//         if (JSON.stringify(layout.value) !== JSON.stringify(uiSchema.layout)) {
+//             layout.value = uiSchema.layout;
+//         }
+//     }
+// }
+// TODO create formfields directly
 </script>
 
 <template>
-    <FormWrap
-        class="vjf_object"
-        :id="id"
-        v-if="layout"
-        :key="layoutElement.scope"
-        :layout-element="layout"
-    />
+    <template>
+        <fieldset :class="cssClass">
+            <legend v-show="label">
+                {{ label }}
+                <!--            <span style="font-size: 1rem">-->
+                <!--                <component :is="HelpPopover" />-->
+                <!--            </span>-->
+            </legend>
+            <p v-if="jsonElement.description">
+                {{ jsonElement.description }}
+            </p>
+            <vertical-layout
+                class="vjf_fieldset-content"
+                :layout-element="{
+                    ...subUiSchema.layout,
+                    type: 'VerticalLayout',
+                }"
+            />
+        </fieldset>
+    </template>
+
+    <style scoped lang="scss">
+        legend {
+            display: flex;
+            align-items: center;
+        }
+    </style>
 </template>
 
 <style scoped></style>
