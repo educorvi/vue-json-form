a7867790998d8bd41a29f5989ff7afc2438697fe
Henrik Herzig <henrik.herzig@educorvi.de>
2026-05-20 09:11:46 +0200

    chore: add gitignore and small fix

--- apps/json-forms-builder/nuxt.config.ts
+++ apps/json-forms-builder/nuxt.config.ts
@@ -1,5 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import Aura from '@primeuix/themes/aura';
+import { UVNexusPreset } from '@educorvi/uv-theme';
 
 export default defineNuxtConfig({
     compatibilityDate: '2025-07-15',
