7652950fe6ee13f5cd01fae48e18774b0f275842
Henrik Herzig <henrik.herzig@educorvi.de>
2026-07-03 15:31:00 +0200

    fix: import of json forms builder package

--- apps/json-forms-builder/app/pages/form-builder/index.vue
+++ apps/json-forms-builder/app/pages/form-builder/index.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
 definePageMeta({ middleware: ['authenticated'], layout: 'base-layout' });
 // import { useTodoStore } from '@/store/todo';
-import FormBuilderApp from '@educorvi/vue-json-form-builder/src/App.vue';
-
+// import FormBuilderApp from '@educorvi/vue-json-form-builder/src/App.vue';
+import VueJsonFormBuilder from '@educorvi/vue-json-form-builder';
 // const store = useTodoStore();
 // store.addTodo({ text: 'Test todo', id: '5', done: false, important: false });
 // store.addTodo({ text: 'Test todo', id: '5', done: false, important: false });
@@ -11,7 +11,8 @@ import FormBuilderApp from '@educorvi/vue-json-form-builder/src/App.vue';
 
 <template>
     <div class="h-100 overflow-hidden">
-        <FormBuilderApp />
+        <!-- <FormBuilderApp /> -->
+        <VueJsonFormBuilder />
         <!-- <div>{{ store.todos }}</div>
         <div>Hallo</div> -->
     </div>
--- packages/form-builder-component/src/main.ts
+++ packages/form-builder-component/src/main.ts
@@ -1,22 +1,26 @@
-import { createApp } from 'vue';
-import { createPinia } from 'pinia';
-import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
-import { createBootstrap } from 'bootstrap-vue-next';
+// import { createApp } from 'vue';
+// import { createPinia } from 'pinia';
+// import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
+// import { createBootstrap } from 'bootstrap-vue-next';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap-vue-next/dist/bootstrap-vue-next.css';
-import 'bootstrap-icons/font/bootstrap-icons.css';
-import '@educorvi/vue-json-form/dist/vue-json-form.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap-vue-next/dist/bootstrap-vue-next.css';
+// import 'bootstrap-icons/font/bootstrap-icons.css';
+// import '@educorvi/vue-json-form/dist/vue-json-form.css';
 
-import App from './App.vue';
-import './assets/main.css';
+// import App from './App.vue';
+// import './assets/main.css';
 
-const pinia = createPinia();
-pinia.use(piniaPluginPersistedstate);
+// const pinia = createPinia();
+// pinia.use(piniaPluginPersistedstate);
 
-const app = createApp(App);
+// const app = createApp(App);
 
-app.use(pinia);
-app.use(createBootstrap());
+// app.use(pinia);
+// app.use(createBootstrap());
 
-app.mount('#app');
+// app.mount('#app');
+
+import VueJsonFormBuilder from './App.vue';
+export { VueJsonFormBuilder };
+export default VueJsonFormBuilder;
