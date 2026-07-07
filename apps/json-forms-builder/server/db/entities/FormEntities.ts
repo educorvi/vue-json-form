f1659e0074e7ac004d2ae42b7ce2383630a315de
Nina Mücke <nina.muecke@educorvi.de>
2026-07-07 13:27:16 +0200

    fix: wizard hierarchy and type errors in form entities

--- apps/json-forms-builder/server/db/entities/FormEntities.ts
+++ apps/json-forms-builder/server/db/entities/FormEntities.ts
@@ -8,49 +8,72 @@ import { Layouts, StringFormats, BooleanFormats, EnumFormats, HttpsMethods, Butt
 // TODO take entity as base class from henrik (basetimestampedentity) instead
 export abstract class DatabaseEntity {
     @PrimaryGeneratedColumn()
-    id: number;
+    id!: number;
 
     @Column()
-    created: Date;
+    created!: Date;
 
     @Column()
-    updated: Date;
+    updated!: Date;
 }
 
 @Entity()
 export class Dependency extends DatabaseEntity {
     @ManyToOne(() => SimpleElement, (simpleElement) => simpleElement.partOfDependency)
-    source: SimpleElement; // on which element the target depends
+    source!: SimpleElement; // on which element the target depends
 
     @Column()
-    dependencyType: DependencyTypes;
+    dependencyType!: DependencyTypes;
 
     @Column()
-    value: number | string | boolean; // the value to compare to, e.g. for "greaterThan", the value that the source should be greater than; for "contains", the value that should be contained in the source; etc.
+    value!: number | string | boolean; // the value to compare to, e.g. for "greaterThan", the value that the source should be greater than; for "contains", the value that should be contained in the source; etc.
 
-    @ManyToOne(() => DependencyGroup, (dependencyGroup) => dependencyGroup.dependencies)
-    dependencyGroup: DependencyGroup;
+    @ManyToOne(
+        () => DependencyGroup,
+        (dependencyGroup) => dependencyGroup.deps
+    )
+    dependencyGroup!: DependencyGroup;
 }
 
 @Entity()
 export class DependencyGroup extends DatabaseEntity {
-    @OneToMany(() => Dependency | DependencyGroup, (dependency) => dependency.dependencyGroup)
-    dependencies: Dependency[] | DependencyGroup []; // TODO how does it work that both types are possible?
+    @OneToMany(
+        () => Dependency,
+        (dependency) => dependency.dependencyGroup,
+        { nullable: true }
+    )
+    deps!: Dependency[];
+
+    @OneToMany(
+        () => DependencyGroup,
+        (dep_group) => dep_group.parentDepGroup,
+        { nullable: true }
+    )
+    depGroups!: DependencyGroup[];
+
+    @ManyToOne( // back reference for dep_groups
+        () => DependencyGroup,
+        (dep_group) => dep_group.depGroups,
+        { nullable: true }
+    )
+    parentDepGroup?: DependencyGroup; // the parent dependency group, if this is a nested dependency group
+
+    // in the final json/ui schema: generate one list that consists of all dependencies and dependency groups (combined with the specified relation)
 
     @Column()
-    dependencyRelation: DependencyRelation; // "AND" or "OR"
+    relation!: DependencyRelation; // "AND" or "OR"
 }
 
 export abstract class FormElement extends DatabaseEntity {
     /*
     class for input and ui elements
     */
-    @ManyToOne(() => ContainerElement, (containerElement) => containerElement.children)
-    parent: ContainerElement;
+    @ManyToOne(() => ContainerElement, (containerElement) => containerElement.children, { nullable: true })
+    parent?: ContainerElement;
 
-    @OneToOne(() => DependencyGroup)
+    @OneToOne(() => DependencyGroup, { nullable: true })
     @JoinColumn()
-    dependencyGroup: DependencyGroup;
+    dependencyGroup?: DependencyGroup;
 }
 
 export abstract class BaseDataElement extends FormElement {
@@ -58,7 +81,7 @@ export abstract class BaseDataElement extends FormElement {
     class for all input elements like string/checkboxes/... and array/object
     */
     @Column()
-    title: string;
+    title!: string;
 
     @Column({ nullable: true })
     description?: string;
@@ -67,7 +90,7 @@ export abstract class BaseDataElement extends FormElement {
     tooltip?: string;
 
     @Column()
-    hidden: boolean;
+    hidden!: boolean;
 
     @Column({ nullable: true })
     preHtml?: string; // TODO remove if htmlelement is allowed inside of array/object
@@ -78,7 +101,7 @@ export abstract class BaseDataElement extends FormElement {
 
 export abstract class SimpleElement extends BaseDataElement {
     @Column()
-    required: boolean;
+    required!: boolean;
 
     @Column({ nullable: true })
     appendValue?: string;
@@ -97,19 +120,19 @@ export abstract class SimpleElement extends BaseDataElement {
 
 export abstract class ContainerElement extends BaseDataElement {
     @OneToMany(() => FormElement, (formElement) => formElement.parent)
-    children: FormElement[]; // TODO how to save the order of the children?
+    children!: FormElement[]; // TODO how to save the order of the children?
 
     @Column()
-    layout: Layouts;
+    layout!: Layouts;
 
     @Column()
-    showTitle: boolean;
+    showTitle!: boolean;
 }
 
 @Entity()
 export class ArrayElement extends ContainerElement {
     @Column()
-    required: boolean; // TODO maybe move to basedataelement (and object also has required in database, but not in ui. object can currently be the source )
+    required!: boolean; // TODO maybe move to basedataelement (and object also has required in database, but not in ui. object can currently be the source )
 
     // constraint >= 0
     @Column({ nullable: true })
@@ -125,19 +148,18 @@ export class ArrayElement extends ContainerElement {
 
 @Entity()
 export class ObjectElement extends ContainerElement {
-    @ManyToOne(() => Wizard, (wizard) => wizard.forms, { nullable: true })
-    wizard?: Wizard; // if object is a form that is used in a wizard
+    // TODO back reference to wizard page (optional)
 }
 
 @Entity()
 export class StringElement extends SimpleElement {
     @Column()
-    format: StringFormats;
+    format!: StringFormats;
 
     // constraint >= 0
     @Column({ nullable: true })
     minLength?: number;
-    
+
     // constraint >=0 and minLength < maxLength
     @Column({ nullable: true })
     maxLength?: number;
@@ -163,24 +185,24 @@ export class NumberElement extends SimpleElement {
 @Entity()
 export class BooleanElement extends SimpleElement {
     @Column()
-    format: BooleanFormats;
+    format!: BooleanFormats;
 
     @Column()
-    mustBeTrue: boolean;
+    mustBeTrue!: boolean;
 }
 
 export abstract class SelectionElement extends SimpleElement {
     @Column()
-    useIdInSchema: boolean;
+    useIdInSchema!: boolean;
 
-    @Column() // TODO save with id-title pairs
-    selectionOptions: string[]; // for select and radio, the options are the possible values; for checkbox group, the options are the labels of the checkboxes
+    @Column({ nullable: true }) // TODO save with id-title pairs
+    selectionOptions?: string[]; // for select and radio, the options are the possible values; for checkbox group, the options are the labels of the checkboxes
 }
 
 @Entity()
 export class EnumElement extends SelectionElement {
     @Column()
-    format: EnumFormats;
+    format!: EnumFormats;
 }
 
 @Entity()
@@ -212,48 +234,48 @@ export class FileuploadElement extends SimpleElement {
 export class ReferenceElement extends FormElement {
     // id of the referenced element (of type )
     @ManyToOne(() => FormElement)
-    referenceId: number;
+    referenceId!: number;
 }
 
 @Entity()
 export class HTMLElement extends FormElement {
     @Column()
-    htmlText: string;
+    htmlText!: string;
 }
 
 @Entity()
 export class ModalElement extends FormElement {
     @Column()
-    title: string;
+    title!: string;
 
     @Column()
-    content: string;
+    content!: string;
 
     @Column({ default: ModalSizes.Medium })
-    size: ModalSizes;
+    size!: ModalSizes;
 
     @Column()
-    buttonLabel: string;
+    buttonLabel!: string;
 
     @Column({ default: ButtonVariants.Primary })
-    variant: ButtonVariants;
+    variant!: ButtonVariants;
 }
 
 @Entity()
 export class ButtonGroup extends FormElement {
     @OneToMany(() => Button, (button) => button.buttonGroup)
-    buttons: Button[]; // TODO how to save the order of the buttons?
+    buttons!: Button[]; // TODO how to save the order of the buttons?
 }
 
 export abstract class Button extends FormElement {
     @Column()
-    label: string;
+    label!: string;
 
     @Column()
-    disabled: boolean;
+    disabled!: boolean;
 
     @Column()
-    variant: ButtonVariants;
+    variant!: ButtonVariants;
 
     // TODO: if button is in a buttongroup and not in an array/object/form, then what is the parent? (parent must be a container so an array or object)
     @ManyToOne(() => ButtonGroup, (buttonGroup) => buttonGroup.buttons, { nullable: true })
@@ -268,13 +290,13 @@ export class ResetButton extends Button {
 @Entity()
 export class SubmitButton extends Button {
     @Column()
-    submitAction: ButtonSubmitActions;
+    submitAction!: ButtonSubmitActions;
 
     @Column()
-    submitUrl: string;
+    submitUrl!: string;
 
     @Column({ default: HttpsMethods.Post })
-    submitMethod: HttpsMethods;
+    submitMethod!: HttpsMethods;
 
     @Column({ nullable: true })
     requestHeaders?: string; // JSON string of key-value pairs
@@ -286,12 +308,30 @@ export class SubmitButton extends Button {
 }
 
 @Entity()
-export class Wizard extends DatabaseEntity {
-    @OneToMany(() => ObjectElement, (objectElement) => objectElement.wizard)
-    forms: ObjectElement[]; // TODO how to save the order of the forms?
+export class WizardPage extends DatabaseEntity { // TODO or extend from object element and dont reference a form?
+    @ManyToOne(() => Wizard, (wizard) => wizard.wizardPages)
+    wizard!: Wizard;
+
+    @OneToOne(() => ObjectElement, (objectElement) => objectElement.id)
+    form!: ObjectElement; // the form that is displayed on this page of the wizard
 
     @Column()
-    pageTitles: string[]; // TODO instead create class WizardPage (which extends object) and has pageTitle attribute. to guarantee that there are as many pagetitles as there are forms
+    pageTitle!: string;
+
+    @Column()
+    position!: number; // position of the page in the wizard (starting from 0)
+}
+
+@Entity()
+export class Wizard extends DatabaseEntity {
+    @OneToMany(() => WizardPage, (wizardPage) => wizardPage.wizard)
+    wizardPages!: WizardPage[];
+
+    @Column()
+    title!: string;
+
+    @Column({ nullable: true })
+    description?: string;
 }
 
 
